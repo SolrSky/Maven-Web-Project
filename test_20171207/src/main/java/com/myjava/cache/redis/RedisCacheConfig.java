58a5527f7444d110ec2f093cd5780a06c60b49e0
dingjunjie <295846891@qq.com>
2017-12-09 12:37:30 +0800

    redis配置

---
+++ test_20171207/src/main/java/com/myjava/cache/redis/RedisCacheConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.myjava.cache.redis;
+
+import org.springframework.cache.annotation.CachingConfigurationSelector;
+
+/**
+ * @author dingjunjie
+ *
+ */
+public class RedisCacheConfig extends CachingConfigurationSelector{
+
+	
+}
